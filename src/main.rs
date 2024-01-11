4088e59e5361568c4651274d06347bcac4cfb482
vitalvirtue <hyterdem@hotmail.com>
2024-01-12 00:47:50 +0300

    added implementation Task with add_task function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Task {
-    _id: i32,
+    _id: usize,
     _description: String,
     _is_completed: bool,
-    _tasks: Vec<i32>,
+    _tasks: Vec<Task>,
 }
 
+impl Task {
+    fn _add_task(&mut self, _description: &str) -> Task {
+        let new_task = Task {
+            _id: self._tasks.len() + 1,
+            _description: String::from(_description),
+            _is_completed: false,
+            _tasks: self._tasks.clone(),
+        };
+        self._tasks.push(new_task.clone());
+        new_task
+    }
+}
 
 
 
